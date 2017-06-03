be05a2a9a82f3d96d0d1b55dcc09823e81474fce
srd <870306552@qq.com>
2017-06-03 17:07:44 +0800

    test commit 5

--- javatest/src/Package1/Test.java
+++ javatest/src/Package1/Test.java
@@ -9,6 +9,8 @@ public class Test {
 		System.out.println("test2");
 		System.out.println("test3");
 		System.out.println("test4");
+		System.out.println("test5	");
+		System.out.println("test6	1234567890");
 	}
 
 }
